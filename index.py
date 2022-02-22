01a34ab9841f7bceaf20624cb5bd797f64a491c4
AnkitSharmaSonu <sssonusharma1999@gmail.com>
2022-02-22 23:18:44 +0530

    integrate sauce lab config

--- index.py
+++ index.py
@@ -1,13 +1,49 @@
 from selenium import webdriver
+# import saucelabs
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 import os
 import configparser
+import sauceclient
+
+# sc = sauceclient.SauceClient(
+#     'oauth-sharmasaket715-5b096',
+#     'f376acc5-18a9-4968-8982-17355233ffe3',
+# )
+# jobs = sc.jobs.get_jobs(full=True, limit=5)
+# print("here is the job", jobs)
+
+desired_cap ={
+    "browserName" : "chrome",
+    "platform" : "windows 10"
+}
+
+sauce_username = os.environ.get("oauth-sharmasaket715-5b096",None)
+sauce_key = os.environ.get("f376acc5-18a9-4968-8982-17355233ffe3" , None)
+URL = "http://{}:{}@ondemand.saucelabs.com:80/wd/hub".format(
+    sauce_username , sauce_key
+)
+driver = webdriver.Remote(command_executor= URL , desired_capabilities= desired_cap)
 
 
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
+# driver = webdriver.Chrome(executable_path="chromedriver.exe")
 driver.get("https://www.skechers.in/")
-action = ActionChains(driver);
+action = ActionChains(driver)
+options = ChromeOptions()
+options.browser_version = 'latest'
+options.platform_name = 'Windows 11'
+options.headless = True
+
+sauce_options = {'username': os.environ["oauth-sharmasaket715-5b096"],
+                 'accessKey': os.environ["f376acc5-18a9-4968-8982-17355233ffe3"]
+                 }
+
+options.set_capability('sauce:options', sauce_options)
+sauce_url = "https://www.skechers.in/"
+
+driver = webdriver.Remote(command_executor=sauce_url, options=options)
 driver.maximize_window()
 time.sleep(2)
 
@@ -54,7 +90,7 @@ def login():
                 "//*[@id='login-form-password']").send_keys("8562SonU@")
             clickLogin = driver.find_element_by_xpath(
                 "//*[@id='login']/form/div[5]/button").click()
-            time.sleep(2)
+            time.sleep(4)
 
     except Exception as error:
         print("here is the error -->>>>", error)
@@ -69,47 +105,55 @@ def addToCart():
         time.sleep(1)
         clickMen = driver.find_element_by_xpath("//*[@id='MensRunningShoes']")
         clickMen.click()
-        checkTextMen = driver.find_element_by_xpath("//*[@id='product-search-results']/div[1]/div[1]/span[1]").text
-       
+        checkTextMen = driver.find_element_by_xpath(
+            "//*[@id='product-search-results']/div[1]/div[1]/span[1]").text
+
         if checkTextMen == "Running Shoes":
-           
-            for i in range(1,10):
-                if i==1 or i==2:
+
+            for i in range(1, 10):
+                if i == 1 or i == 2:
                     print("")
                 else:
-                    i+=1
+                    i += 1
 
                 time.sleep(1)
-                clickShoes = driver.find_element_by_xpath(f"(//div[@class='main-image-slider-item pt-3 pt-md-2']){[i]}")
+                clickShoes = driver.find_element_by_xpath(
+                    f"(//div[@class='main-image-slider-item pt-3 pt-md-2']){[i]}")
                 print(f" {i} time")
                 clickShoes.click()
-                for j in range(1,7):
-                   
-                    clickShoeSize = driver.find_element_by_xpath(f'//*[@id="size-1.0"]/button{[j]}')
+                for j in range(1, 7):
+
+                    clickShoeSize = driver.find_element_by_xpath(
+                        f'//*[@id="size-1.0"]/button{[j]}')
                     clickShoeSize.click()
-                    time.sleep(2)
-                    cart = driver.find_element_by_xpath("//*[@id='maincontent']/div[2]/div[3]/div[2]/div[5]/div[11]/div/div/div/div[1]/div/button").text
-                  
+                    time.sleep(3)
+                    cart = driver.find_element_by_xpath(
+                        "//*[@id='maincontent']/div[2]/div[3]/div[2]/div[5]/div[11]/div/div/div/div[1]/div/button").text
+
                     if cart == "ADD TO CART":
-                        addInCart = driver.find_element_by_xpath("//*[@id='maincontent']/div[2]/div[3]/div[2]/div[5]/div[11]/div/div/div/div[1]/div/button")
+                        addInCart = driver.find_element_by_xpath(
+                            "//*[@id='maincontent']/div[2]/div[3]/div[2]/div[5]/div[11]/div/div/div/div[1]/div/button")
                         addInCart.click()
-                        time.sleep(1)
-                        clickMenShoes = driver.find_element_by_xpath("//*[@id='maincontent']/div[2]/div[1]/div/div/div/ol/li[3]/a")
+                        time.sleep(2)
+                        clickMenShoes = driver.find_element_by_xpath(
+                            "//*[@id='maincontent']/div[2]/div[1]/div/div/div/ol/li[3]/a")
                         clickMenShoes.click()
                         break
-        
-    except Exception as error :
+
+    except Exception as error:
         print("here is the error -->>>>", error)
         driver.quit()
 
 # MAIN FUNCTION
+
+
 def main():
     try:
 
         intro()
         login()
         addToCart()
-    except Exception as error :
+    except Exception as error:
         print("here is the error -->>>>", error)
         driver.quit()
 
